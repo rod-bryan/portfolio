ebf86f14cda1b94557c0d48ba2325b147d4a4342
unknown <rodbryanpagsanjan@outlook.com>
2024-03-24 22:28:53 +0800

    updated main page name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,13 +12,13 @@ const Main = () => {
             />
             <div className='w-full h-screen absolute top-0 left-0 bg-gray-500/50'>
                 <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center'>
-                    <h1 className='sm:text-5xl text-4xl font-bold text-gray-300'>I'm Cris Carlo</h1>
+                    <h1 className='sm:text-5xl text-4xl font-bold text-gray-300'>I'm Rod-Bryan</h1>
                     <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-300'>
                         I'm a
                         <TypeAnimation
                             sequence={[
                                 // Same substring at the start will only be typed out once, initially
-                                'Senior QA Automation Engineer',
+                                'QA Software Engineer',
                                 2000, // wait 1s before replacing "Mice" with "Hamsters"
                                 'Tech Enthusiast',
                                 2000
